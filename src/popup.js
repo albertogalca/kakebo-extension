6b9b302903674eef18c48ca38dedfd54bf794ec7
Alberto Gallego <albertogc2793@gmail.com>
2020-12-05 22:39:26 +0100

    popup wip

--- src/popup.js
+++ src/popup.js
@@ -1,11 +1,30 @@
 import 'libs/polyfills';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ThemeProvider } from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import defaultTheme from 'themes/default';
 
+import { Paragraph, H2 } from './components';
+
+const Box = styled.div`
+  min-width: 300px;
+  padding: 38px 11px 38px 10px;
+  box-shadow: 0 11px 23px 0 rgba(0, 0, 0, 0.18);
+  border: solid 0.5px #979797;
+  background-color: #ffffff;
+`;
+
 const Popup = () => {
-  return <ThemeProvider theme={defaultTheme}></ThemeProvider>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Box>
+        <H2>Ahorra con el Método Kakebo</H2>
+        <Paragraph>
+          ¡Oinc, oinc! Soy el cerdito ahorrador y voy a vigilar todos tus movimientos para ayudarte.
+        </Paragraph>
+      </Box>
+    </ThemeProvider>
+  );
 };
 
 const root = document.createElement('div');
