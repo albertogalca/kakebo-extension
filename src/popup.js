9f01057bf62e09fccd6c1981739e5d9c9536c7f6
Alberto Gallego <albertogc2793@gmail.com>
2020-12-10 12:25:56 +0100

    styled popup

--- src/popup.js
+++ src/popup.js
@@ -1,29 +1,65 @@
 import 'libs/polyfills';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import styled, { ThemeProvider } from 'styled-components';
-import defaultTheme from 'themes/default';
+import styled, { createGlobalStyle } from 'styled-components';
 
 import { Paragraph, H2 } from './components';
 
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin:0px !important;
+    border-radius: 5px !important;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-rendering: optimizeLegibility;
+    -moz-font-feature-settings: "liga" on;
+  }
+`;
+
 const Box = styled.div`
   min-width: 300px;
-  padding: 38px 11px 38px 10px;
+  padding: 32px 24px;
   box-shadow: 0 11px 23px 0 rgba(0, 0, 0, 0.18);
-  border: solid 0.5px #979797;
   background-color: #ffffff;
+`;
+
+const Container = styled.div`
+  width: 90%;
+  margin: 0 auto;
+`;
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Logo = styled.img`
+  margin-right: 24px;
+`;
+
+const StyledParagraph = styled(Paragraph)`
+  text-align: center;
 `;
 
 const Popup = () => {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Box>
-        <H2>Ahorra con el Método Kakebo</H2>
-        <Paragraph>
+    <Box>
+      <Container>
+        <GlobalStyle />
+        <Header>
+          <Logo src="https://i.imgur.com/EFaR68V.png" width="50" alt="logo" />
+          <H2>Ahorra con el Método Kakebo</H2>
+        </Header>
+
+        <StyledParagraph>
           ¡Oinc, oinc! Soy el cerdito ahorrador y voy a vigilar todos tus movimientos para ayudarte.
-        </Paragraph>
-      </Box>
-    </ThemeProvider>
+        </StyledParagraph>
+      </Container>
+    </Box>
   );
 };
 
